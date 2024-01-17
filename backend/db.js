e4fcd6bf6cc957952827f6f69df77a7696ef6464
Siddhi <siddhi11.aswale@gmail.com>
2024-01-17 20:30:46 +0530

    MongoDB and express seerver setup

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,13 @@
+/*Mongoose is an Object Data Modeling (ODM) library for MongoDB and Node.js. It manages relationships between data, 
+provides schema validation, and is used to translate between objects in code and the representation of those objects in MongoDB.*/
+
+// the MongoDB URI (Uniform Resource Identifier), which specifies the location of the MongoDB database.
+
 const mongoose = require('mongoose');
 const mongoURI = "mongodb://localhost:27017/";    //if i want to deploy this project somewhere else then i will change this uri
 
+//This line declares an asynchronous function named connectToMongo. 
+//The async keyword is used because Mongoose operations are asynchronous and return promises.
 const connectToMongo = async () => {
   try {
     await mongoose.connect(mongoURI);
@@ -12,12 +19,6 @@ const connectToMongo = async () => {
   }
 };
 
-// const mongoose = require('mongoose');
-// const mongoURI = "mongodb://localhost:27017/"
-// const connectToMongo = () => {
-//     mongoose.connect(mongoURI,()=>{
-//         console.log("Connected to Mongo Successfully");
-// })
-// }
-
+//the connectToMongo function is exported from the module 
+//so that it can be used in other parts of your application.
 module.exports = connectToMongo;
